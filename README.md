e5d15215980dcd34e6d85d3ab85a13c1814d1785
Zachary Friss <zfriss@hubspot.com>
2014-07-05 16:32:06 -0400

    Update bus icon and add green line stops code clean up. Update readme to point at new repos

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 bostontraintrack
 ================
 
+Update by Zachary Friss
+-------
+I have extended this to show buses and predictions for trains.
+
+I am working on a rewrite and continued development here:
+[Server](https://github.com/Friss/BostonTransitServer)
+[Website](https://github.com/Friss/BostonTransitWeb)
+
+
 Simple real time tracking of Boston metros using Open Street Map (via OpenLayers) and MBTA's real time metro tracking.
 
 See it live at [bostontraintrack.com](http://bostontraintrack.com).
+
 
 LICENSE
 -------
--- srv/srv.js
+++ srv/srv.js
@@ -6,153 +6,160 @@ var g_verbose = 1;
 
 var display_bus = true;
 
-var global_connect = { };
+var global_connect = {};
 
-var global_data = { n: 0,
-                    interval : { red:10000, blue:13000, orange:17000, bus:12000 },
-                    port : 80,
-                    time: 0,
-                    url : "developer.mbta.com",
-                    busURL: "webservices.nextbus.com",
-                    path : { red : "/lib/rthr/red.json",
-                             blue: "/lib/rthr/blue.json",
-                             orange: "/lib/rthr/orange.json",
-                             bus: "/service/publicXMLFeed?command=vehicleLocations&a=mbta&t=" }
-                  };
+var global_data = {
+  n: 0,
+  interval: {
+    red: 10000,
+    blue: 13000,
+    orange: 17000,
+    bus: 12000
+  },
+  port: 80,
+  url: "developer.mbta.com",
+  busURL: "webservices.nextbus.com",
+  path: {
+    red: "/lib/rthr/red.json",
+    blue: "/lib/rthr/blue.json",
+    orange: "/lib/rthr/orange.json",
+    bus: "/service/publicXMLFeed?command=vehicleLocations&a=mbta&t=0"
+  }
+};
 
 for (var rbo in global_data.path) {
 
-  console.log("setting up " + rbo );
+  console.log("setting up " + rbo);
 
   if (rbo != "bus") {
     setInterval(
-      (function(RBO) {
+      (function (RBO) {
 
-        return function()  {
-          if (g_verbose) { console.log("upating " + RBO + " -->>>>"); }
-          var opt = { host: global_data.url, port:global_data.port, path: global_data.path[RBO] };
+        return function () {
+          if (g_verbose) {
+            console.log("upating " + RBO + " -->>>>");
+          }
+          var opt = {
+            host: global_data.url,
+            port: global_data.port,
+            path: global_data.path[RBO]
+          };
 
           try {
 
-
-            //console.log("cp0");
-
-            var req = http.request( opt, function(res) {
+            var req = http.request(opt, function (res) {
               res.setEncoding('utf8');
               var body = '';
-              res.on('data', function(chunk) { body += chunk; });
-              res.on('end', function() {
+              res.on('data', function (chunk) {
+                body += chunk;
+              });
+              res.on('end', function () {
 
-                //console.log(body);
-
-                try  {
+                try {
                   jr = JSON.parse(body);
                   global_data[RBO] = jr;
+                  console.log(RBO + " Trains: " + jr.TripList.Trips.length)
                 } catch (ee) {
-                  console.log("json parse error:", ee );
+                  console.log("json parse error:", ee);
                 }
 
               });
             });
 
-            req.on('error', function(err) { console.log("got http erro:", err); });
+            req.on('error', function (err) {
+              console.log("got http erro:", err);
+            });
             req.end();
 
-            //console.log("cp1");
           } catch (error) {
             console.log("http: got e:", error);
           }
 
         };
 
-      })(rbo), global_data.interval[rbo] );
-  }else if(display_bus){
+      })(rbo), global_data.interval[rbo]);
+  } else if (display_bus) {
     setInterval(
-      (function(RBO) {
+      (function (RBO) {
 
-        return function()  {
-          if (g_verbose) { console.log("updating " + RBO + " -->>>>"); }
-          var opt = { host: global_data.busURL, port:global_data.port, path: global_data.path[RBO] + global_data.time };
+        return function () {
+          if (g_verbose) {
+            console.log("updating " + RBO + " -->>>>");
+          }
+          var opt = {
+            host: global_data.busURL,
+            port: global_data.port,
+            path: global_data.path[RBO]
+          };
 
           try {
 
-            //console.log("Updating Bus");
-
-            var req = http.request( opt, function(res) {
-              res.setEncoding('utf8');
-              var body = '';
-              res.on('data', function(chunk) { body += chunk; });
-              res.on('end', function() {
-
-                parseString(body, function (err, result) {
-                  //console.log(result)
-                 global_data[RBO] = result;
-
-                 global_data.time = (new Date).getTime();
-
+            var req = http.request(opt, function (res) {
+                res.setEncoding('utf8');
+                var body = '';
+                res.on('data', function (chunk) {
+                  body += chunk;
                 });
-              });
-            });
+                res.on('end', function () {
 
-            req.on('error', function(err) { console.log("got http erro:", err); });
-            req.end();
+                    parseString(body, function (err, result) {
+                        global_data[RBO] = result;
 
-            //console.log("cp1");
-          } catch (e) {
-            console.log("http: got e:", e);
-          }
+                        console.log("Buses: " + result.body.vehicle.length
 
-        };
+                        });
+                    });
+                });
 
-      })(rbo), global_data.interval[rbo] );
+              req.on('error', function (err) {
+                console.log("got http erro:", err);
+              }); req.end();
+
+            } catch (e) {
+              console.log("http: got e:", e);
+            }
+
+          };
+
+        })(rbo), global_data.interval[rbo]);
+    }
   }
-}
 
+  sockio.on('connection', function (socket) {
+    console.log("connection!");
 
-sockio.on('connection', function(socket) {
-  console.log("connection!");
+    global_data.n++;
+    var local_name = global_data.n;
+    global_connect[local_name] = socket;
 
-  global_data.n++;
-  var local_name = global_data.n;
-  global_connect[ local_name ] = socket;
+    socket.on("myevent", function (msg) {
+      console.log("got myevent!");
+      console.log(msg);
 
+      socket.emit("update", {
+        n: global_data.n
+      });
+    });
 
-  socket.on("myevent", function(msg) {
-    console.log("got myevent!");
-    console.log(msg);
+    socket.on("disconnect", function () {
+      console.log("disconnecting client ", local_name);
+      delete global_connect[local_name];
 
-    socket.emit("update", { n: global_data.n } );
+    });
+
   });
 
-  socket.on("disconnect", function() {
-    console.log("disconnecting client ", local_name);
-    delete global_connect[ local_name ];
+  sockio.listen(8181);
 
-  });
-
-});
-
-//sockio.on('disconnect', function(socket) { console.log("disconnect"); } );
-//sockio.on('myevent', function(socket) { console.log("myevent!"); } );
-
-sockio.listen(8181);
-
-setInterval( function() {
-  //console.log(global_data.n);
-  for (var cli_id in global_connect) {
-    var colors = [ "red", "orange", "blue", "bus" ];
-
-    for (var i in colors) {
-      if (colors[i] in global_data) {
-        var dat = {}
-        dat[ colors[i] ] = global_data[ colors[i] ];
-        global_connect[ cli_id ].emit("update", dat );
+  setInterval(function () {
+    for (var cli_id in global_connect) {
+      var colors = ["red", "orange", "blue", "bus"];
+      for (var i in colors) {
+        if (colors[i] in global_data) {
+          var dat = {}
+          dat[colors[i]] = global_data[colors[i]];
+          global_connect[cli_id].emit("update", dat);
+        }
       }
     }
-
-  }
-}, 1000 );
-
-
-
-
+  }, 1000);
